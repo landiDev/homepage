aa37cc5994969a027c01ddbeea2668a4b1faa29c
Adin <40150678+landiDev@users.noreply.github.com>
2025-02-15 11:11:35 +0100

    Update config.js

--- config.js
+++ config.js
@@ -20,7 +20,7 @@ const CONFIG = {
 	greetingMorning: 'Good morning!',
 	greetingAfternoon: 'Good afternoon,',
 	greetingEvening: 'Good evening,',
-	greetingNight: 'Go to Sleep!',
+	greetingNight: 'Good Night!',
 
 	// Layout
 	bentoLayout: 'bento', // 'bento', 'lists', 'buttons'
